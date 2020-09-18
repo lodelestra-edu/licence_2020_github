d46f5f5890ae3af48e8ada23f23f319ff8368f22
AllanJRY <jarry.allan@gmail.com>
2020-09-18 10:46:17 +0200

    Rebase - Ajout du film Gladiator

--- pages/movies.js
+++ pages/movies.js
@@ -23,7 +23,12 @@ const movies = [
     synopsis: "Pellentesque mauris elit, vulputate eu sagittis ut, molestie vitae orci. Integer rutrum, ex vitae sodales sagittis, risus neque consequat libero, eget lacinia orci nibh et turpis.",
     ratings: 5,
   },
-    ]
+  {
+    title: "Gladiator",
+    synopsis: "Le général romain Maximus est le plus fidèle soutien de l'empereur Marc Aurèle, qu'il a conduit de victoire en victoire. Jaloux du prestige de Maximus, et plus encore de l'amour que lui voue l'empereur, le fils de Marc Aurèle, Commode, s'arroge brutalement le pouvoir, puis ordonne l'arrestation du général et son exécution. Maximus échappe à ses assassins, mais ne peut empêcher le massacre de sa famille. Capturé par un marchand d'esclaves, il devient gladiateur et prépare sa vengeance.",
+    ratings: 5,
+  },
+]
 
 const Movies = () => {
   return (
