9a16db654916c54327ab3ffb9b9d72be21648823
Killian <killian.dherment@gmail.com>
2020-09-22 00:19:49 +0200

    Correction bug syntaxique empechant compilation

--- pages/movies.js
+++ pages/movies.js
@@ -33,6 +33,7 @@ const movies = [
     synopsis: "Sans aucun doute le meilleur film jamais créé.",
     ratings: 5,
   },
+  {
     title: "Gladiator",
     synopsis: "Le général romain Maximus est le plus fidèle soutien de l'empereur Marc Aurèle, qu'il a conduit de victoire en victoire. Jaloux du prestige de Maximus, et plus encore de l'amour que lui voue l'empereur, le fils de Marc Aurèle, Commode, s'arroge brutalement le pouvoir, puis ordonne l'arrestation du général et son exécution. Maximus échappe à ses assassins, mais ne peut empêcher le massacre de sa famille. Capturé par un marchand d'esclaves, il devient gladiateur et prépare sa vengeance.",
     ratings: 5,
